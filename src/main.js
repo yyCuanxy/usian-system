577ca225ffe4f1e2c45991b7aa14d38b599327a9
yyCuanxy <2822843586@qq.com>
2022-09-19 20:41:33 +0800

    chore: 安装element、axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
+// 引入element
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+// 注册elementui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
